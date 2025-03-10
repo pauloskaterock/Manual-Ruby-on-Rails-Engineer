845789421a05b983c606b62ce04c1327443a32fd
pauloskaterock <pauloengenharia5@yahoo.com>
2025-03-10 15:31:41 -0300

    Update

--- 100-exerciciosLogica-ruby/exercicio11.rb
+++ 100-exerciciosLogica-ruby/exercicio11.rb
@@ -1,2 +1,39 @@
 # Escreva um programa que conte de 1 a 10.
 # 
+
+# Contador de 1 a 10 em Ruby
+
+1.upto(10) do |i|
+    puts i
+end
+
+
+# Explicação linha por linha:
+# 1.upto(10) do |i|:
+
+# 1.upto(10) é um método em Ruby que itera de 1 até 10, incluindo o 10.
+
+# do |i| inicia um bloco de código que será executado para cada valor de i (que vai de 1 a 10).
+
+# i é a variável que armazena o valor atual da iteração.
+
+# puts i:
+
+# puts é um método em Ruby que imprime o valor de i na tela e adiciona uma nova linha após a impressão.
+
+# i é o valor atual da iteração, que será impresso na tela.
+
+# end:
+
+# end fecha o bloco de código que foi iniciado com do.
+
+# Isso indica que o loop terminou e o programa continuará a execução após este ponto (se houver mais código).
+
+# Como funciona:
+# O programa começa com i = 1 e executa o bloco de código dentro do do...end.
+
+# A cada iteração, o valor de i é impresso na tela.
+
+# O loop continua até que i atinja o valor 10.
+
+# Após a última iteração, o programa termina.
